d4405a5f5f5da619dc58ff5fad6e487aab1f18d2
kyle <kyle.cha@kakaohealthcare.com>
2023-08-30 11:01:11 +0900

    apply dependency

--- Package.swift
+++ Package.swift
@@ -3,20 +3,30 @@
 import PackageDescription
 
 let package = Package(
-    name: "KakaoHealthcareCalendar",
-    platforms: [
-        .iOS(.v14)
-    ],
-    products: [
-        .library(
-            name: "KakaoHealthcareCalendar",
-            targets: ["KakaoHealthcareCalendar"]
-        )
-    ],
-    targets: [
-        .target(
-            name: "KakaoHealthcareCalendar",
-            path: "Sources"
-        )
-    ]
+  name: "KakaoHealthcareCalendar",
+  platforms: [
+    .iOS(.v14)
+  ],
+  products: [
+    .library(
+      name: "KakaoHealthcareCalendar",
+      targets: ["KakaoHealthcareCalendar"]
+    )
+  ],
+  dependencies: [
+    .package(
+      url: "https://github.com/WenchaoD/FSCalendar.git",
+      .upToNextMajor(from: "2.8.3")
+    )
+  ],
+  targets: [
+    .target(
+      name: "KakaoHealthcareCalendar",
+      dependencies: [
+        .product(name: "FSCalendar", package: "FSCalendar")
+      ],
+      path: "Sources"
+      
+    )
+  ]
 )
